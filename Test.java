cdb7c1610f63cc9a4cb06b48085041c32284eb55
ChenZiShi <18222670829@163.com>
2016-12-27 21:17:04 +0800

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Test {
-	public static void main(String[] args) throws IOException {
-	LoginInterface x=new LoginInterface();
-	SellerInterface a=new SellerInterface("shop1");
-	}
-
-}
