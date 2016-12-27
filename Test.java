90eeb69d6a764e3ef3bd07db25c745a7e52c5c4b
ChenZiShi <18222670829@163.com>
2016-12-27 21:09:11 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Test {
+	public static void main(String[] args) throws IOException {
+	LoginInterface x=new LoginInterface();
+	SellerInterface a=new SellerInterface("shop1");
+	}
+
+}
